bd79fd0c583e11b753bf22a2c98d59820c5352e6
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2024-05-20 20:04:06 +0200

    back on track

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { CheckCircleIcon } from "@heroicons/react/16/solid";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home !!!</h1>
+    <main className="h-screen flex items-center justify-center flex-row p-8">
+      <div className="space-x-4">
+        <button
+          type="button"
+          className="inline-flex items-center gap-x-2 rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 m-2"
+        >
+          Log in
+          <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center gap-x-2 rounded-md bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 m-2"
+        >
+          Sign in
+          <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+        </button>
+      </div>
     </main>
   );
 }
